e571d7f7b96bf9794c568050f4c426c6e195d21e
Antonio Camera <antonio.camera@hotmail.com>
2024-09-01 10:14:16 +0200

    init soluzione

---
+++ MauiCemSwBtTest/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiCemSwBtTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiCemSwBtTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiCemSwBtTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
